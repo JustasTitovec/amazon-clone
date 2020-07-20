d4275924511a98704a913705a259cae20c15c5da
Justas Titovec <justastitovec@Justass-MacBook-Pro.local>
2020-07-20 16:59:04 +0300

    added more products

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,14 @@ import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
 import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function Header() {
-  const [{ basket }] = useStateValue();
+  const [{ basket, user }] = useStateValue();
+
+  const login = user => {
+    auth.signOut();
+  };
 
   return (
     <nav className="header">
@@ -25,10 +30,12 @@ function Header() {
       </div>
       {/* 3 links */}
       <div className="header__nav">
-        <Link to="/login" className="header__link">
-          <div className="header__option">
-            <span className="header__optionLineOne">Hello</span>
-            <span className="header__optionLineTwo">Sign In</span>
+        <Link to={!user && '/login'} className="header__link">
+          <div onClick={login} className="header__option">
+            <span className="header__optionLineOne">Hello {user?.email}</span>
+            <span className="header__optionLineTwo">
+              {user ? 'Sign out' : 'Sign in'}
+            </span>
           </div>
         </Link>
         <Link to="/" className="header__link">
